0ce3f0765957823191e36af69c0133ec5ddf9588
IvBican182 <ibicanic@live.com>
2024-01-31 11:03:29 +0100

    final changes

--- src/display_lyrics.ts
+++ src/display_lyrics.ts
@@ -4,7 +4,7 @@ export function displayLyrics(data: string) {
     const lyrics = document.getElementById('lyrics') as HTMLInputElement;
     
     
-    //lyrics.value = "";
+   
     if (!data) {
         lyrics.innerHTML = "Oops! Seems like we don't have that one yet :(";
     } else {
